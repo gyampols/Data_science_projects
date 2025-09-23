9de39d8b7d93dd47590e06fdd10df09c8e492c62
gyampols <gregyampolsky@gmail.com>
2025-09-22 20:10:23 -0400

    folder structure change

--- split-test-marketing/src/tests/test_file_load.py
+++ split-test-marketing/src/tests/test_file_load.py
@@ -1,6 +1,6 @@
 import unittest 
 
-from DATA_SCIENCE_PROJECTS/split-test-marketing/src import train
+from src import train
 
 class TestModelMetrics(unittest.TestCase):
   def test_file_load(self):
